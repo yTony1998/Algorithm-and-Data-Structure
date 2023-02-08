45bae2ee03b90157e480cd0d4dd4b47a81e4abae
yTony1998 <562928435@qq.com>
2023-02-08 11:52:53 +0800

    StackLinked

---
+++ CPratice/C++DataStructure/StackLinked.hpp
@@ -0,0 +1,61 @@
+#ifndef STACKLINKED
+#define STACKLINKED
+
+#include <assert.h>
+template <class Type> class Stack;
+
+template <class Type> class StackNode
+{
+friend class Stack <Type>
+private:
+    Type data;
+    StackNode<Type> *link;
+    StackNode(Type d=0, StackNode<Type> *l = NULL):data(d), link(l){}
+
+};
+
+template <class Type> class Stack
+{
+public:
+    Stack():top(NULL){}
+    ~Stack();
+    void Push(const Type &item);
+    Type Pop();
+    Type GetTop();
+    void MakeEmpty(){~Stack();}
+    int IsEmpty() const {return top == NULL;}
+
+private:
+    StackNode<Type> *top;
+};
+
+template <class Type> Stack<Type>::~Stack()
+{
+    StackNode<Type> *p;
+    while(top != NULL)
+    {
+        p =top; top = top->link; delete p;
+    }
+}
+template <class Type> void Stack<Type>::Push(const Type &item)
+{
+    top = new StackNode<Type> (item, top);
+}
+
+template <class Type> Type Stack<Type>::Pop()
+{
+    assert(!IsEmpty());
+    StackNode<Type> *p = top;
+    Type retvalue = p->data;
+    top = top->link;
+    delete p;
+    return retvalue;
+}
+
+template <class Type> Type Stack<Type>::GetTop()
+{
+    assert(!IsEmpty());
+    return top->data;
+}
+
+#endif STACKLICKED
